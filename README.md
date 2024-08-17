d922a9b74b233d3356ddbec7523a9a211cc2c49d
MLazalde <mikealazalde@gmail.com>
2024-08-16 22:26:56 -0600

    learning string manipulation

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # python_learning
+
+## Description
+
+- I would like to deepen my coding knowledge so I want to cement my understanding of how to code by learning Python. Since I am already somewhat proficient in Javascript, I want to translate those fundamental concepts to another popular learning language. This will help the concepts take root in my memory by learning them in another syntax.
+
+This is how bilingual folks are able to learn better! :D
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+- none. These files will mostely be for my own notes.
+
+## License
+
+![GitHub License](https://img.shields.io/badge/license-None-blue.svg)
+
+## Contributing
+
+None
+
+## Tests
+
+None
+
+## Questions
+
+GitHub Repository: [GitHub]()
+
+If you have any questions, please email me at <Mikealazalde@gmail.com>.
--- learningpython.py
+++ learningpython.py
@@ -1,15 +1,31 @@
-print('Rise & Shine!')
+# print('Rise & Shine!')
 
-message="Variable in python"
-print(message)
+# message="Variable in python"
+# print(message)
 
-# methods already included
-#.title upper cases first char in word of string
-name='corina cortez'
-print(name.title())
+# # methods already included
+# # .title upper cases first char in word of string
+# name='corina cortez'
+# print(name.title())
 
-#.upper method uppercases each char in string
-print(name.upper())
+# # .upper method uppercases each char in string
+# print(name.upper())
 
-#.lower method lower cases each char in string
-print(name.lower())
+# # .lower method lower cases each char in string
+# print(name.lower())
+
+# first_name='warden'
+# last_name='lazalde'
+# full_name=f'{first_name} {last_name}'
+# print(full_name)
+# # this is called an 'f-string' (f for format)
+# # much like JQuery, use the curley braces which you can then store in a new variable
+
+# print(f'Hello, {full_name.title()}')
+# message=f'Hey, {full_name.title()}'
+# print(message)
+
+# to add a tab to your text, use \t and \n for a new line
+# print('Languages:\n\tPython\n\tJavascript\n\tC')
+
+import this
