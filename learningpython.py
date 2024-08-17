83b67cbc9e73b1a91623f126e084aa1ae193f5f9
MLazalde <mikealazalde@gmail.com>
2024-08-16 22:04:13 -0600

    adding practice files

---
+++ learningpython.py
@@ -0,0 +1,15 @@
+print('Rise & Shine!')
+
+message="Variable in python"
+print(message)
+
+# methods already included
+#.title upper cases first char in word of string
+name='corina cortez'
+print(name.title())
+
+#.upper method uppercases each char in string
+print(name.upper())
+
+#.lower method lower cases each char in string
+print(name.lower())
